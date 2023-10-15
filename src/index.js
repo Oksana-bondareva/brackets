eb0b6d64272497f6810b7bf4693058761a19eaee
Oksana-bondareva <oksanchik252@gmail.com>
2023-10-15 18:55:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let stack = [];
+    let open = [];
+    let close = [];
+    let equally = [];
+
+    bracketsConfig.map((el) => {
+      if (el[0] === el[1]) {
+        equally += el[0];
+      } else {
+        open += el[0];
+        close += el[1];
+      }
+    });
+
+    for (let i = 0; i < str.length; i++) {
+      let closeInd = close.indexOf(str[i]);
+      let equallyInd = equally.indexOf(str[i]);
+      if (open.includes(str[i])) {
+        stack.push(str[i]);
+      } else if (close.includes(str[i])) {
+        if (stack.length === 0) false;
+        if (stack[stack.length - 1] === open[closeInd]) {
+          stack.pop();
+        } else {
+          stack.push(str[i]);
+        }
+      } else if (equally.includes(str[i])) {
+        if (stack[stack.length - 1] === equally[equallyInd]) {
+          stack.pop();
+        } else {
+          stack.push(str[i]);
+        }
+      }
+    }
+
+  return stack.length === 0;
+  };
